998b89e64a517adfa2540ed5cf16324fe11eac67
Pavan Sarvesh <pavansarvesh31@gmail.com>
2024-06-19 15:11:30 +0530

    First Commit

---
+++ 01_helloWorld.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+// Contract name should start from Capital Letter
+contract MyFirstContract {
+    string public hey = "Hello World";  // public keyword creates a getter function
+}
