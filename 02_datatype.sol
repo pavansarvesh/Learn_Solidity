5e9528fc752fc32cb17fd49686b14339e6d6d3ba
Pavan Sarvesh <pavansarvesh31@gmail.com>
2024-06-22 21:25:09 +0530

    save

--- 02_datatype.sol
+++ 02_datatype.sol
@@ -13,5 +13,19 @@ contract Datatype {
     uint8 num; // range = 0 to 2 ** 8 - 1 = 255
     uint16 num1; // range = 0 to 2 ** 16 - 1 = 65535
 
-    
+    int public minInt = type(int).min; //Displays the minimum number of datatype int
+    int public maxInt = type(int).max; //Displays the maximum number of datatype int
+
+
+    // ARRAY
+    //* Fixed size
+    //* Dynamic size
+
+    bytes1 public a;
+    // Assigning values
+    bytes1 public b = 0x56;
+
+
+    // Address Datatype
+    address myAddr = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
 }
